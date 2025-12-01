0bbf10d88a5ebef854a9e23f7f9fb5d384ce6dd1
Jasonbackwood <Jason@Backwood.biz>
2025-12-01 16:40:39 -0700

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,8 @@
+import SocialLinks from "./SocialLinks";
+
+...
+
+<footer className="text-center py-10 text-gray-400">
+  <p>© {new Date().getFullYear()} Backwood Illuminated LLC</p>
+  <SocialLinks className="mt-4 justify-center" />
+</footer>
