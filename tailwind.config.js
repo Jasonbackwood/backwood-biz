eca0b49966a7d88dde18f0d37538b535f33fceaa
Jasonbackwood <Jason@Backwood.biz>
2025-11-30 21:38:35 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./styles/**/*.{css}"],
-  theme: { extend: {} },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
