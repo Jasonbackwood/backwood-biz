96d8bbc430ead8893bec2c708012274f78c20bd6
Jasonbackwood <Jason@Backwood.biz>
2025-12-01 17:00:53 -0700

    Update SocialLinks.jsx

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -1,16 +1,40 @@
-"use client";
-
 export default function SocialLinks({ className = "" }) {
   return (
-    <div className={`flex gap-4 ${className}`}>
-      <a href="https://www.facebook.com/profile.php?id=100090937422770" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/facebook.svg" className="w-6 h-6 hover:opacity-60 transition" />
+    <div className={`flex gap-6 ${className}`}>
+      <a
+        href="https://www.facebook.com/profile.php?id=100090937422770"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="/icons/facebook.svg"
+          alt="Facebook"
+          className="w-8 h-8 hover:opacity-80"
+        />
       </a>
-      <a href="https://www.instagram.com/backwood_illuminated_/" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/instagram.svg" className="w-6 h-6 hover:opacity-60 transition" />
+
+      <a
+        href="https://www.instagram.com/backwood_illuminated_/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="/icons/instagram.svg"
+          alt="Instagram"
+          className="w-8 h-8 hover:opacity-80"
+        />
       </a>
-      <a href="https://www.tiktok.com/@backwood.illumina2" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/tiktok.svg" className="w-6 h-6 hover:opacity-60 transition" />
+
+      <a
+        href="https://www.tiktok.com/@backwood.illumina2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="/icons/tiktok.svg"
+          alt="TikTok"
+          className="w-8 h-8 hover:opacity-80"
+        />
       </a>
     </div>
   );
