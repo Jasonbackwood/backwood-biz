eaa696533505bf36ac596fccbc923d8464bd320d
Jasonbackwood <Jason@Backwood.biz>
2025-12-05 14:44:22 -0700

    Update SocialLinks.jsx

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -2,40 +2,34 @@ import Image from "next/image";
 
 export default function SocialLinks() {
   return (
-    <div className="flex items-center justify-center space-x-6 mt-6">
-      <a
-        href="https://www.facebook.com/profile.php?id=100090937422770"
-        target="_blank"
-      >
+    <div className="flex gap-4 justify-center mt-4">
+      <a href="https://www.instagram.com/backwoodilluminated" target="_blank" rel="noopener noreferrer">
         <Image
-          src="/social/facebook.png"
-          alt="Facebook"
-          width={40}
-          height={40}
-        />
-      </a>
-
-      <a
-        href="https://www.instagram.com/backwood_illuminated_/"
-        target="_blank"
-      >
-        <Image
-          src="/social/instagram.png"
+          src="/social/instagram_opt.webp"
           alt="Instagram"
           width={40}
           height={40}
+          className="hover:opacity-80 transition"
         />
       </a>
 
-      <a
-        href="https://www.tiktok.com/@backwood.illumina2"
-        target="_blank"
-      >
+      <a href="https://www.tiktok.com/@backwoodilluminated" target="_blank" rel="noopener noreferrer">
         <Image
-          src="/social/tiktok.png"
+          src="/social/tiktok_opt.webp"
           alt="TikTok"
           width={40}
           height={40}
+          className="hover:opacity-80 transition"
+        />
+      </a>
+
+      <a href="https://www.facebook.com/backwoodilluminated" target="_blank" rel="noopener noreferrer">
+        <Image
+          src="/social/facebook_opt.webp"
+          alt="Facebook"
+          width={40}
+          height={40}
+          className="hover:opacity-80 transition"
         />
       </a>
     </div>
